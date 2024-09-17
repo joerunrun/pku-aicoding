4a5475680b953c5a189e742ab2fda4e3e28d0099
rungui zhou <2300011366@stu.pku.edu.cn>
2024-09-17 17:18:06 +0800

    mnist:1

--- mnist.py
+++ mnist.py
@@ -1,2 +1,17 @@
 import torch 
 import torch.nn as nn
+import torch.optim as optim
+
+
+
+class net(nn.Module):
+    def _init_(self):
+        super(net,self)._init_()
+        self.flatten=nn.Flatten()
+        self.f=nn.Sequential(nn.Linear(28*28,150),nn.Sigmoid(),nn.Linear(150,512),nn.Sigmoid(),nn.Linear(512,10))
+
+    def forward(self,x):
+            x=self.flatten(x)
+            x=self.f(x)
+            return x
+menet=net()   
